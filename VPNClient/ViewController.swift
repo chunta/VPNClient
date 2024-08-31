757f20d90a32dac91b3519346ac4d763b8696677
rex <rex>
2024-08-31 13:36:19 +0800

    init commit

---
+++ VPNClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VPNClient
+//
+//  Created by Rex Chen on 2024/8/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
