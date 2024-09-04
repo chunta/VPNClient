2fdb2eaea25bf04ab7d585cf4f1d2c1d1b30ae3a
rex <rex>
2024-09-04 18:44:19 +0800

    have basic implementation

--- ContentExt/NotificationViewController.swift
+++ ContentExt/NotificationViewController.swift
@@ -10,16 +10,25 @@ import UserNotifications
 import UserNotificationsUI
 
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
-
+    
     @IBOutlet var label: UILabel?
+    @IBOutlet var imageVw: UIImageView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any required interface initialization here.
     }
     
     func didReceive(_ notification: UNNotification) {
         self.label?.text = notification.request.content.body
+        
+        let attachements = notification.request.content.attachments
+        for attachement in attachements {
+            if attachement.identifier == "picture" {
+                guard let data = try? Data(contentsOf: attachement.url) else {
+                    return
+                }
+                imageVw?.image = UIImage(data: data)
+            }
+        }
     }
-
 }
--- ServiceExt/NotificationService.swift
+++ ServiceExt/NotificationService.swift
@@ -7,29 +7,63 @@
 
 import UserNotifications
 
-class NotificationService: UNNotificationServiceExtension {
+import UserNotifications
 
+class NotificationService: UNNotificationServiceExtension {
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
-
+    
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
+        
+        guard let bestAttemptContent = bestAttemptContent,
+              let attachmentURLAsString = bestAttemptContent.userInfo["url"] as? String,
+              let attachmentURL = URL(string: attachmentURLAsString) else {
+            return
+        }
+        
+        let iconName = bestAttemptContent.userInfo["icon_name"] as? String {
             
+        }
+        
+        // 4. Download the image and pass it to attachments if not nil
+        downloadImageFrom(url: attachmentURL) { attachment in
+            if let attachment = attachment {
+                bestAttemptContent.attachments = [attachment]
+            }
             contentHandler(bestAttemptContent)
         }
     }
     
-    override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
+    private func downloadImageFrom(url: URL, with completionHandler: @escaping (UNNotificationAttachment?) -> Void) {
+        let task = URLSession.shared.downloadTask(with: url) { (downloadedUrl, response, error) in
+            // 1. Test URL and escape if URL not OK
+            guard let downloadedUrl = downloadedUrl else {
+                completionHandler(nil)
+                return
+            }
+            
+            // 2. Get current user's temporary directory path
+            var urlPath = URL(fileURLWithPath: NSTemporaryDirectory())
+            
+            // 3. Add proper ending to URL path, in the case .jpg
+            let uniqueURLEnding = ProcessInfo.processInfo.globallyUniqueString + ".jpg"
+            urlPath = urlPath.appendingPathComponent(uniqueURLEnding)
+            
+            // 4. Move downloadedUrl to newly created urlPath
+            try? FileManager.default.moveItem(at: downloadedUrl, to: urlPath)
+            
+            // 5. Try adding the attachment and pass it to the completion handler
+            do {
+                let attachment = try UNNotificationAttachment(identifier: "picture", url: urlPath, options: nil)
+                completionHandler(attachment)
+            } catch {
+                completionHandler(nil)
+            }
         }
+        task.resume()
     }
-
+    
 }
